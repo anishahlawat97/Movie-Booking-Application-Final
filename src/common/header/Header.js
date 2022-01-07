7bafccbb1869e25dad4fda2d4cadfd0eba8cad09
Anish <anish.mi619@gmail.com>
2022-01-07 17:35:28 +0530

    checkpoint 1 -converted header to functional component, added login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,32 +1,24 @@
-import React from "react";
-import { Component } from "react";
+import React, { useContext} from "react";
 import { ReactComponent as ReactLogo } from '../../assets/logo.svg';
 import './Header.css';
 import AppContext from "../../AppContext";
 import { Button } from "@mui/material";
 
 
-class Header extends Component{  
-   
-    constructor(props){
-        super(props); 
-        this.loginHandler= this.loginHandler.bind(this);
-    }  
-    loginHandler(){
-        (this.context.setLoginValue(!this.context.isLogin))
+function Header(){
+    const context = useContext(AppContext);  
+
+    function loginHandler(){
+        (context.setLoginValue(!context.isLogin))
     }   
-
-    static contextType = AppContext;    
-    render(){
-        return(                 
-        <div className="header">  
-            {this.context.bookShow?<Button sx={{color: 'primary', width: '9%', position: 'absolute', right: '15%'}} variant="contained">Book Show</Button>:null } 
-            {this.context.isLogin?<Button sx={{color: 'primary', width: '9%', position: 'absolute', right: '5%'}} variant="contained" onClick={this.loginHandler}>Logout</Button>:<Button sx={{color: 'primary', width: '9%', position: 'absolute', right: '5%'}} onClick={this.loginHandler} variant="contained">Login</Button>}    
-           <ReactLogo className="logo"/>
-        </div>  
-        );     
-    }
+    
+    return(                 
+    <div className="header">  
+        {context.bookShow?<Button sx={{color: 'primary', width: '9%', position: 'absolute', right: '15%'}} variant="contained">Book Show</Button>:null } 
+        {context.isLogin?<Button sx={{ width: '9%', position: 'absolute', right: '5%'}} variant="contained" onClick={loginHandler}>Logout</Button>:<Button sx={{width: '9%', position: 'absolute', right: '5%'}} onClick={loginHandler} variant="contained">Login</Button>}    
+        <ReactLogo className="logo"/>
+    </div>  
+    );  
 }
-
 
 export default Header;
